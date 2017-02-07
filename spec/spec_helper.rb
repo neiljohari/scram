babd6bb080f5d47b48b71bc070e0bab8367e1264
skreem <cskreem@gmail.com>
2017-02-07 17:43:01 -0500

    Set up mongoid and database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,20 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "bouncer"
+require 'database_cleaner'
+
+Mongoid.load!('./spec/config/mongoid.yml', :test)
+
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+end
