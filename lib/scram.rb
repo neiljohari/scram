4d5a3a836c0844818c4293ddd8b2a8cdb065d77c
Neil Johari <neil@johari.tech>
2017-02-09 14:46:12 -0500

    Get holder spec running

--- lib/scram.rb
+++ lib/scram.rb
@@ -1,7 +1,7 @@
 require "scram/version"
 require "mongoid"
-require "scram/concerns/holder.rb"
 require "scram/concerns/cannable.rb"
+require "scram/concerns/holder.rb"
 require "scram/app/models/permission_node.rb"
 require "scram/app/models/policy.rb"
 require "scram/app/models/target.rb"
--- lib/scram/concerns/holder.rb
+++ lib/scram/concerns/holder.rb
@@ -2,7 +2,11 @@ module Scram
   module Holder
     extend ActiveSupport::Concern
 
-    
+    def self.included(base)
+      base.include Cannable
+      base.cannable pass_to: :policies
+    end
+
     def policies
       raise NotImplementedError
     end
