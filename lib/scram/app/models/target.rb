790f90d20aceb205be65d125d134ea2107d85ed6
Neil Johari <neil@johari.tech>
2017-02-09 14:24:46 -0500

    Change node early return logic, and add targetting for document states

--- lib/scram/app/models/target.rb
+++ lib/scram/app/models/target.rb
@@ -3,11 +3,21 @@ module Scram
     include Mongoid::Document
 
     embedded_in :permission_node
-    field :collection, type: String
 
-    def can? action, target=nil, *args
-      return true if self.collection == target.class.name
-      return false
+    field :collection, type: String
+    field :match_states, type: Hash, default: {}
+
+    validates_presence_of :collection
+
+    def can? action, target, *args
+      return false if action.to_s != permission_node.name.to_s # This node doesn't apply at all
+
+      if match_states.empty? # This target is a broad collection based ability
+        return true if self.collection == target.class.name
+      else
+        model = self.collection.constantize
+        return model.where(match_states).include? target # Figure out if every model this target applies to includes the parameter target
+      end
     end
 
   end
--- spec/scram/target_spec.rb
+++ spec/scram/target_spec.rb
@@ -1,25 +1,38 @@
 require "spec_helper"
 
 describe Scram::Target do
+  # A test model that we apply our targets to
   class Model
     include Mongoid::Document
     field :targetable_int, type: Integer, default: 3
   end
 
-  let(:target) {FactoryGirl.build(:target)}
+  let(:target) { FactoryGirl.build(:target) }
 
   it "targets a collection" do
+    # Allow node to "blorgh" any "Model"
     target.collection = "Model"
-    target.permission_node.name = :anything # This can be anything we want, since we're specifying that this target applies to the entire collection
+    target.permission_node.name = :blorgh
     target.permission_node.save # save parent to trigger save embeds
 
     obj = Model.new
-    expect(target.can? :blorgh, obj).to eq(true)
+    expect(target.permission_node.can? :blorgh, obj).to eq(true)
+    expect(target.permission_node.can? :dargh, obj).to eq(false)
   end
 
 
-  xit "targets document states" do
+  it "targets document states" do
+    # Allow node to "blorgh" any "Model" where targetable_int = 5
+    target.collection = "Model"
+    target.match_states = {targetable_int: 5}
+    target.permission_node.name = :blorgh
+    target.permission_node.save # save parent to trigger save embeds
 
+    obj = Model.create(targetable_int: 2)
+    expect(target.can? :blorgh, obj).to eq(false) # We cannot blorgh a Model with targetable_int = 2!
+
+    obj1 = Model.create(targetable_int: 5)
+    expect(target.can? :blorgh, obj1).to eq(true) # We can blorgh a Model with targetable_int = 5 though
   end
 
 end
