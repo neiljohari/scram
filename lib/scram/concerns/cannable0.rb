ed773eeaa42f5098068ade5e840f8bc629373a6b
Neil Johari <neil@johari.tech>
2017-02-09 02:02:18 -0500

    Add Cannable spec, add temporary new Cannable implementation (broken)

---
+++ lib/scram/concerns/cannable0.rb
@@ -0,0 +1,75 @@
+module Cannable0
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    private
+      attr_writer :pass_to, :module_to_mix
+    public
+      attr_reader :pass_to, :module_to_mix
+
+    def cannable plurality=:single, pass_to:
+      self.pass_to = pass_to
+      delegate :can?, to: pass_to
+      self.module_to_mix = plurality
+    end
+  end
+
+  def initialize(*args)
+    define_cannable
+    super
+  end
+
+  def define_cannable
+    class << self # Eigen class (object's special class instance)
+      # define pass_to without_permissions
+      # preserve the original method
+      alias_method "#{pass_to}_without_permissions", pass_to
+
+      permission_check = case plurality
+      when :single then Single.can?(self, *args)
+      when :collection then send("#{pass_to}_permission_check", *args)
+      end
+
+      # define pass_to with permissions
+      permission_check ? super : reject
+
+      define_method "#{pass_to}_permission_check" do |*args|
+        Collection.can?(*args)
+      end
+
+      # Both permissions and execution
+      define_method pass_to do |*args|
+        if send("#{pass_to}_permission_check")
+          send("#{pass_to}_without_permission") # continue with execution
+        else
+          raise StandardError # TODO Error
+        end
+      end
+
+    end
+  end
+
+  def pass_to
+    self.class.pass_to
+  end
+
+  def module_to_mix
+    self.class.module_to_mix
+  end
+
+  module Single
+    def self.can?(object, *args)
+      if pass_to
+        send(pass_to, *args)
+      else
+        super()
+      end
+    end
+  end
+
+  module Collection
+    def self.can?(object, *args)
+      all? {|n| n.can? *args }
+    end
+  end
+end
--- spec/scram/concerns/cannable_spec.rb
+++ spec/scram/concerns/cannable_spec.rb
@@ -1,9 +1,62 @@
 require "spec_helper"
 
 module Scram
-  class Foo
+  class TopLevelFoo
     include Cannable
+    cannable :collection, pass_to: :midlevelfoos
+
+    attr_accessor :midlevelfoos
+
+    def initialize
+      @midlevelfoos = [MidLevelFoo.new, MidLevelFoo.new]
+    end
   end
 
+  class MidLevelFoo
+    include Cannable
+    cannable pass_to: :final_foo
 
+    attr_accessor :final_foo
+
+    def initialize
+      @final_foo = FinalFoo.new
+    end
+  end
+
+  class FinalFoo
+    def can? *args
+      true
+    end
+  end
+
+  describe Cannable do
+    let(:top_level_foo) {TopLevelFoo.new}
+
+    def test_on_cannables
+      %i(TopLevelFoo MidLevelFoo).each do |foo|
+          yield(Object.const_get("Scram::#{foo}").new)
+      end
+    end
+
+
+    it "is includable" do
+      test_on_cannables do |foo|
+        expect(foo).to be_a(Cannable)
+      end
+    end
+
+    it "responds to can?" do
+      test_on_cannables do |foo|
+        expect(foo).to respond_to(:can?)
+      end
+    end
+
+    context TopLevelFoo do
+      it "chains the can? method down" do
+        expect(top_level_foo.can? :wonk, :donk).to eq(true)
+      end
+    end
+
+
+  end
 end
