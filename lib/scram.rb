5040ca3bd1733d0d19da7800a1286f2bfda564fc
skreem <cskreem@gmail.com>
2017-02-18 16:42:44 -0500

    Rework hierarchy for permissions again

--- lib/scram.rb
+++ lib/scram.rb
@@ -3,6 +3,7 @@ require "mongoid"
 require "scram/dsl.rb"
 require "scram/concerns/holder.rb"
 require "scram/app/models/policy.rb"
+require "scram/app/models/permission_node.rb"
 require "scram/app/models/target.rb"
 
 module Scram
--- lib/scram/app/models/target.rb
+++ lib/scram/app/models/target.rb
@@ -1,8 +1,16 @@
 module Scram
   class Target
     include Mongoid::Document
-    embedded_in :policy
+    embedded_in :permission_node
 
     field :conditions, type: Hash, default: {}
+
+    def can? obj
+      if obj.is_a? String
+        return obj == conditions[:equals][:$target_name]
+      else
+        # TODO: Comparators
+      end
+    end
   end
 end
