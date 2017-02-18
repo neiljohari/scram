5d101d106054c3c06112bf7e1a67b6a62b7660ad
Neil Johari <neil@johari.tech>
2017-02-18 15:56:50 -0500

    Remove permission node model, flatten into array of strings within policy

--- lib/scram.rb
+++ lib/scram.rb
@@ -2,7 +2,6 @@ require "scram/version"
 require "mongoid"
 require "scram/dsl.rb"
 require "scram/concerns/holder.rb"
-require "scram/app/models/permission_node.rb"
 require "scram/app/models/policy.rb"
 require "scram/app/models/target.rb"
 
