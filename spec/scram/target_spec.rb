09233a41a892d99d7db03b427fbec43cbfc9206c
skreem <cskreem@gmail.com>
2017-02-19 15:40:40 -0500

    Refactor test implementations, fix errors for unknown fields

---
+++ spec/scram/target_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+module Scram
+  describe Scram::Target do
+    it "returns false when a field doesn't apply" do
+      policy = Policy.new
+      policy.collection_name = TestModel.name
+
+      target = Target.new
+      target.actions << "woot"
+      target.allow = true
+      target.conditions = {:equals => {:non_existant_field => 3}}
+      policy.targets << target
+
+      policy.save
+
+      dude = SimpleHolder.new(policies: [policy]) # This is a test holder
+      expect(dude.can? :woot, TestModel.new).to be false
+    end
+  end
+end
