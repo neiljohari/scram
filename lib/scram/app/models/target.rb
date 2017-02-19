a4e76f1b95bb574ce7496a11f1461a3a19e43e60
skreem <cskreem@gmail.com>
2017-02-19 16:31:14 -0500

    Cleanup Target, add way to invoke condition as a method prefixed with *

--- lib/scram/app/models/target.rb
+++ lib/scram/app/models/target.rb
@@ -32,29 +32,13 @@ module Scram
 
       return false unless actions.include? action
       return false if !allow
-      if obj.is_a? String # ex: can? user, :view, "peek_bar"
+      if obj.is_a? String # Handle String permissions with a simple check on the equals field
         return obj == conditions[:equals][:'*target_name']
-        # ex: conditions: {equals: {@target_name: "peek_bar"}}
-      else
+      else # Attempt to prove false by finding a condition or attribute where comparisons fail
         conditions.each do |comparator_name, fields_hash|
           comparator = Scram::DSL::Definitions::COMPARATORS[comparator_name]
           fields_hash.each do |field, model_value|
-            # equals: {@involved: @holder}
-            # equals: {age: 50}
-            # @ symbol in field name => it is defined as a DSL condition, otherwise it is a model attribute
-            # @ symbol in model_value => a special replace variable
-            field = field.to_s
-
-            attribute = begin
-              if field.starts_with? "*"
-              policy.model.scram_conditions[field.split("*")[1].to_sym].call(obj)
-              else
-                obj.send(field)
-              end
-            rescue
-              return false
-            end
-
+            attribute = begin obj.send(field.to_s) rescue return false end
             model_value.gsub! "*holder", holder.scram_compare_value if model_value.respond_to?(:gsub!)
             return false unless comparator.call(attribute, model_value)
           end
