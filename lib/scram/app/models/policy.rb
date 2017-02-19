5f69133f5bd00ce518087264d43b940e0f7049fe
Neil Johari <neil@johari.tech>
2017-02-19 07:36:44 -0500

    Flatten permission nodes into target

--- lib/scram/app/models/policy.rb
+++ lib/scram/app/models/policy.rb
@@ -3,24 +3,38 @@ module Scram
     include Mongoid::Document
     store_in collection: 'scram_policies'
 
-    embeds_many :permission_nodes
+    embeds_many :targets
 
     validates_presence_of :collection
 
-    field :collection_name, type: String # This is usually a model name, but depending on `model` it could also just be a global policy
-    field :model, type: Boolean, default: true # Is this a Policy affiliated with a model (i.e. is `collection` a Model)?
+    field :collection_name, type: String # This is usually a model name, but depending on #model? it could also just be a global policy
 
-    def can? action, target
+    # Helper method to easily tell if this policy is bound to a model
+    ## Unnecessary since we can just call model.nil?, but it is helpful nonetheless 
+    def model?
+      return !model.nil?
+    end
+
+    # Attempts to constantize and get a model
+    def model
+      begin
+        return Module.const_get(collection_name)
+      rescue NameError
+        return nil
+      end
+    end
+
+    def can? holder, action, obj
       # The following checks prevent unnecessary iteration
 
-      if target.is_a? String # ex: can? :view, "peek_bar"
-        return false if self.model # policy doesn't handle strings
+      if obj.is_a? String # ex: can? :view, "peek_bar"
+        return false if self.model? # policy doesn't handle strings
       else                   # ex: can? :edit, @model_instance
-        return false if !self.model # policy doesn't handle models
-        return false if self.collection_name != target.name # policy doesn't handle these types of models
+        return false if !self.model? # policy doesn't handle models
+        return false if self.collection_name != obj.name # policy doesn't handle these types of models
       end
 
-      return permission_nodes.any? {|node| node.can?(action, target)}
+      return targets.any? {|target| target.can?(holder, action, obj)}
     end
   end
 end
--- lib/scram/concerns/holder.rb
+++ lib/scram/concerns/holder.rb
@@ -10,7 +10,7 @@ module Scram
       target = target.to_s if target.is_a? Symbol
       action = action.to_s if action.is_a? Symbol
 
-      policies.any? {|p| p.can? action, target}
+      policies.any? {|p| p.can? self, action, target}
     end
 
   end
